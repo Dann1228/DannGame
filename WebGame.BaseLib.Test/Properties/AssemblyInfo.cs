afdf4dbb266161dbd40c7f7b3382d0238c1d0cde
Daniel Huang <daniel.huang@hy-tech.com.tw>
2020-01-23 16:42:04 +0800

    [專案] 加入測試專案 及 BaseLib [測試] 測試C#呼叫Python

---
+++ WebGame.BaseLib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebGame.BaseLib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("WebGame.BaseLib.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("255f3602-4087-4e09-bf5c-d2b94b3059eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebGame.BaseLib.Test/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace WebGame.BaseLib.Test
+{
+    public class UnitTest1 
+    {
+        
+        public class BasicPM
+        {
+            public string A { set; get; }
+            public string B { set; get; }
+            public string C { set; get; }
+        }
+
+        /// <summary>
+        /// 呼叫 Python
+        /// </summary>
+        [Test]
+        [TestCase(new object[] { 1}, "1")]
+        [TestCase(new object[] { "12","at","2" }, "14")]
+        public void TestMethod1(object[] arr , string expectResultMessage)
+        {
+            string result = string.Empty;
+            string outputText = string.Empty;
+            string errorMsg = string.Empty;
+            string filePath_python = @"D:\Training\Test_ExchangeRate\Test_ExchangeRate\Test_ExchangeRate.py";
+            //string filePath_pythonExe = @"C:\Program Files(x86)\Microsoft Visual Studio\Shared\Python36_64\python.exe";
+            string filePath_pythonExe = @"python.exe";
+            string arguments = filePath_python;
+            foreach (var arg in arr)
+            {
+                arguments += " "+ arg;
+            }
+            try
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo = new ProcessStartInfo(filePath_pythonExe)
+                    {
+                        Arguments = arguments,
+                        UseShellExecute = false,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true
+                    };
+
+                    process.Start();
+                    outputText = process.StandardOutput.ReadToEnd();
+                    outputText = outputText.Replace(Environment.NewLine, string.Empty);
+                    errorMsg = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                expectResultMessage = ex.Message;
+            }
+
+            if (!string.IsNullOrWhiteSpace(outputText))
+            {
+                result = outputText;
+            }
+
+            Assert.True(expectResultMessage.Equals(result), string.Format("Should show message {0}, but \"{1}\"", expectResultMessage, result.ToString()));
+        }
+    }
+}
